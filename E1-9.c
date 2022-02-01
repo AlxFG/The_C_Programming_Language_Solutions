20724510d2f0fa6eae3ecd8512845762b973d7f3
Alx <alexy6725@gmail.com>
2022-02-01 19:14:11 +0000

    added E1-9

---
+++ E1-9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int c,lc;
+    while ((c=getchar())!=EOF){
+        if (lc==' '&&c==' ');
+        else if (c=='\t')
+            putchar(' ');
+        else
+            putchar(c);
+        lc=c;}
+    return 0;
+}
+
