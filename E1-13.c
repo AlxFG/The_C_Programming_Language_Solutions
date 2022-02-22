1c330309bc193cfc06c0d24f13417d65e5fd4902
Alx <alexy6725@gmail.com>
2022-02-22 21:47:16 +0000

    I made a version of E1-13.c that does vertical output but I get a buffer overflow error if the input is very large

--- E1-13.c
+++ E1-13.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 int main(){
-    int c,wl,width,height;
-    int sizes[10];
-    wl = height = 0;
-    for (c=0; c<10;++c){
-        sizes[c]=0;}
-    while ((c=getchar())!=EOF){
-        if (wl > 10){
-            ++sizes[9];
-            wl = 0;}
-        if (c=='\t'||c=='\n' || c==' '){
-            ++sizes[wl-1];
+    int c,wl,height;
+    int values[10];
+    for (c = 0; c<10;++c){
+        values[c]=0;
+    }
+    for (wl = 0;c!= EOF;c=getchar()){
+        if (c == '\n' || c == ' ' || c =='\t'){
+            ++values[wl-1];
             wl = 0;}
         else
             ++wl;
-        }
+    }
+    for (c = 0; c<10;++c){
+        printf("%d ",values[c]);
+    }
+    putchar('\n');
+    height = 0;
+
     for (c=0; c<10;++c){
-        printf("%3d |",c+1);
-        for(wl = sizes[c]; wl!=0;--wl){
-            putchar('*');}
-        putchar('\n');}
+        if (height<=values[c])
+            height = values[c];
+    }
+
+    for (c=height;c>0;--c){
+        printf("%3d |",c);
+        for (wl=0;wl<10;++wl){
+            if (values[wl]==c){
+                printf(" * ");
+                --values[wl];
+            }
+            else
+                printf("   ");
+        }
+        putchar('\n');
+    }
+    printf("  0 |");
+    for (c=1;c<10;++c){
+        printf(" %d|",c);}
+    printf(" 10+|\n");
     return 0;
 }
