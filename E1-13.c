712f1e21c8e0434faa7cd671d75762ff3c188cba
Alx <alexy6725@gmail.com>
2022-03-04 19:07:31 +0000

    commenting out E1-13.c to try and see if I can find the error

--- E1-13.c
+++ E1-13.c
@@ -2,9 +2,11 @@
 int main(){
     int c,wl,height;
     int values[10];
+    //declaring variables
     for (c = 0; c<10;++c){
         values[c]=0;
     }
+    //assigning all array values with 0
     for (wl = 0;c!= EOF;c=getchar()){
         if (c == '\n' || c == ' ' || c =='\t'){
             ++values[wl-1];
@@ -12,9 +14,11 @@ int main(){
         else
             ++wl;
     }
+    //getting character input and incrementing the position of the corrensponding array's length for the word length
     for (c = 0; c<10;++c){
         printf("%d ",values[c]);
     }
+    //printing out the number of words for each word count
     putchar('\n');
     height = 0;
 
@@ -22,6 +26,7 @@ int main(){
         if (height<=values[c])
             height = values[c];
     }
+    //finding how high the program has to print to
 
     for (c=height;c>0;--c){
         printf("%3d |",c);
@@ -35,9 +40,11 @@ int main(){
         }
         putchar('\n');
     }
+    //printing all the a point for each value of the histogram with * as class width can be 1
     printf("  0 |");
     for (c=1;c<10;++c){
         printf(" %d|",c);}
     printf(" 10+|\n");
+    //printing out the x axist and its labels
     return 0;
 }
